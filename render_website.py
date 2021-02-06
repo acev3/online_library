0eb94db0069517da19b60f46d3d2bb2d4c93495c
acev3 <kovalchukilya5@gmail.com>
2021-02-06 12:18:42 +0300

    Added pages

--- render_website.py
+++ render_website.py
@@ -3,6 +3,7 @@ import json
 from more_itertools import chunked
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
+import math
 
 
 env = Environment(
@@ -15,13 +16,12 @@ def on_reload():
     template = env.get_template('template.html')
     with open('books_info.json') as f:
         books_info = json.load(f)
-    #books_data = list(chunked(books_info, 2))
-    #rendered_page = template.render(chunked_books=books_data)
     os.makedirs("pages/", exist_ok=True)
     books_info = list(chunked(books_info, 10))
+    pages = len(books_info)
     for i, chunked_books in enumerate(books_info, 1):
         books_data = list(chunked(chunked_books, 2))
-        rendered_page = template.render(chunked_books=books_data)
+        rendered_page = template.render(chunked_books=books_data, pages=pages, page=i)
         filename = "index{}.html".format(i)
         filepath = os.path.join("pages/", filename)
         with open(filepath, 'w', encoding="utf8") as file:
