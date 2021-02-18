fb1deefd8385ba9e678564c66a82cf4ede9dec49
acev3 <kovalchukilya5@gmail.com>
2021-02-18 20:39:45 +0300

    Rename variable

--- render_website.py
+++ render_website.py
@@ -17,11 +17,11 @@ def on_reload():
     with open('media/books_info.json') as f:
         books = json.load(f)
     os.makedirs("pages/", exist_ok=True)
-    books = list(chunked(books, 10))
-    pages = len(books)
-    for i, book in enumerate(books, 1):
-        chunked_books = list(chunked(book, 2))
-        rendered_page = template.render(chunked_books=chunked_books, pages=pages, current_page=i)
+    chunked_books = list(chunked(books, 10))
+    pages = len(chunked_books)
+    for i, book in enumerate(chunked_books, 1):
+        double_chunked_books = list(chunked(book, 2))
+        rendered_page = template.render(chunked_books=double_chunked_books, pages=pages, current_page=i)
         filename = "index{}.html".format(i)
         filepath = os.path.join("pages/", filename)
         with open(filepath, 'w', encoding="utf8") as file:
